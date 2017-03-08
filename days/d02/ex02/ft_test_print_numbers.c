284f92d24369e3a0937f75a67b1d0505a49253b6
Clemence BERGON <clbergon@e3r3p11.42.fr>
2017-03-08 21:12:33 +0100

    d02e03

---
+++ days/d02/ex02/ft_test_print_numbers.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_numbers.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: clbergon <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/03/08 17:05:06 by clbergon          #+#    #+#             */
+/*   Updated: 2017/03/08 20:49:30 by clbergon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_numbers(void)
+{
+	int i;
+
+	i = '0';
+	while (i <= '9')
+	{
+		ft_putchar(i);
+		i++;
+	}
+	ft_putchar('\n');
+}
+
+int		main(void)
+{
+	ft_print_numbers();
+	return (0);
+}
---
+++ days/d02/ex03/ft_test_is_negative.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_is_negative.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: clbergon <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/03/08 20:59:59 by clbergon          #+#    #+#             */
+/*   Updated: 2017/03/08 21:05:02 by clbergon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_is_negative(int n)
+{
+	if (n < 0)
+		ft_putchar('N');
+	if (n >= 0)
+		ft_putchar('P');
+}
+
+int		main(void)
+{
+	ft_is_negative(0);
+	ft_is_negative(10);
+	ft_is_negative(-42);
+	return (0);
+}
