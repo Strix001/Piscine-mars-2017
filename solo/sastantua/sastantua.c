a725db36c07cc08629320aa2fd77db89228cc10d
Clemence BERGON <clbergon@e3r3p2.42.fr>
2017-03-12 20:31:55 +0100

    sastantuasansporte

--- solo/sastantua/sastantua.c
+++ solo/sastantua/sastantua.c
@@ -5,34 +5,68 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: clbergon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/03/12 13:56:40 by clbergon          #+#    #+#             */
-/*   Updated: 2017/03/12 14:54:29 by clbergon         ###   ########.fr       */
+/*   Created: 2017/03/12 17:54:25 by clbergon          #+#    #+#             */
+/*   Updated: 2017/03/12 20:31:17 by clbergon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_putchar(char c);
 
+int		largeur_etage_i(int i)
+{
+	if (i == 0)
+		return (7);
+	return (largeur_etage_i(i - 1) + 2 * (1 + (3 + i) + ((i % 2) + (i / 2))));
+}
+
+void	etage(int hauteur, int largeur, int decallage)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (i < hauteur)
+	{
+		j = -decallage;
+		while (j < hauteur - 1 - i)
+		{
+			ft_putchar(' ');
+			j++;
+		}
+		j += hauteur + 1 - i;
+		ft_putchar('/');
+		while (j < largeur)
+		{
+			ft_putchar('*');
+			j++;
+		}
+		ft_putchar(92);
+		ft_putchar('\n');
+		i++;
+	}
+}
+
 void	sastantua(int size)
 {
-	int n;
-	int spaces;
+	int i;
+	int hauteur;
+	int largeur;
+	int decallage;
 
-	spaces = size;	
-	n = size - 1;
-	while (size > 0)
+	i = 0;
+	while (i < size)
 	{
-		while (spaces-- != 0)
-			ft_putchar(' ');
-		int to_print = (n - size) * 2 + 1;
-		while (to_print-- > 0)
-			ft_putchar('*');
-		size--;
-		ft_putchar('\n');
+		decallage = (largeur_etage_i(size - 1) - largeur_etage_i(i)) / 2;
+		hauteur = 3 + i;
+		largeur = largeur_etage_i(i);
+		etage(hauteur, largeur, decallage);
+		if (etage(hauteur, largeur, decallage
+		i++;
 	}
 }
 
 int		main(void)
 {
-	sastantua(6);
+	sastantua(5);
 	return (0);
 }
