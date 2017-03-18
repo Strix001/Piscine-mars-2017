8d217e81cbd1a99ed7cd4a3c514b001ea1f244af
Clemence BERGON <clbergon@e3r6p12.42.fr>
2017-03-18 22:48:45 +0100

    matchnmatch

--- days/d05/ex05/ft_strstr.c
+++ days/d05/ex05/ft_strstr.c
@@ -6,10 +6,44 @@
 /*   By: clbergon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/17 14:36:06 by clbergon          #+#    #+#             */
-/*   Updated: 2017/03/17 14:56:52 by clbergon         ###   ########.fr       */
+/*   Updated: 2017/03/18 21:24:13 by clbergon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+
 char	*ft_strstr(char *str, char *to_find)
 {
-	char 
+	int	i;
+	int	j;
+	int	 size_to_find;
+
+	i = 0;
+	j = 0;
+	size_to_find = 0;
+	while (to_find[size_to_find] != '\0')
+		size_to_find++;
+	if (size_to_find == 0)
+		return (str);
+	while (str[i] != '\0')
+	{
+		while (to_find[j] == str[i + j])
+		{
+			if (j == size_to_find - 1)
+				return (str + i);
+			j++;
+		}
+		j = 0;
+		i++;
+	}
+	return (0);
+}
+
+int		main(void)
+{
+	char	str1[] = "Hello World, poulet in your plate ...";
+	char	*to_find = "poulet";
+
+	printf("%s\n", ft_strstr(str1, to_find));
+	return (0);
+}
